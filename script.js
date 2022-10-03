de6035748b3e71751627c72ffff7389a6c7207cb
daniel kayode <danielodugbemi8@gmail.com>
2022-10-03 17:16:12 +0100

    tetx2

---
+++ script.js
@@ -0,0 +1,9 @@
+const display = document.querySelector('.modal')
+const aworan = document.querySelector('.image')
+aworan.addEventListener('click', () => {
+    aworan.classList.toggle('active')
+    // alert('i am a man')
+})
+
+
+// Element.style.propertyName = "value"
